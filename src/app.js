5d83ac984ddae24785fcfd31e9251ccc30afc594
Pratik <pratikyadav3949@gmail.com>
2024-07-19 10:16:36 +0530

    feat(server) update routes in app file

--- src/app.js
+++ src/app.js
@@ -2,6 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
+const morgan = require('morgan');
+const { errorHandler } = require('./middlewares/error.middlewares.js');
+
+const userRouter = require('./routes/auth/user.routes.js');
+const blogRouter = require('./routes/blog.routes.js');
+const likeRouter = require('./routes/like.routes.js');
 
 const app = express();
 app.use(
@@ -18,5 +24,14 @@ app.use(cookieParser());
 
 // Set security headers with Helmet middleware
 app.use(helmet());
+
+// Log requests with Morgan middleware (use 'combined' format for production)
+app.use(morgan('dev'));
+
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/blogs', blogRouter);
+app.use('/api/v1/likes', likeRouter);
+
+app.use(errorHandler);
 
 module.exports = { app };
